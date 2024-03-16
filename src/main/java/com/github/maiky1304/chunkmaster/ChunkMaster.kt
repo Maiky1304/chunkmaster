0403babb58a156a0c4c9bfa45e56a8c61864e38d
Maiky Perlee <maiky-perlee@outlook.com>
2024-03-16 14:24:26 +0100

    Initial commit

---
+++ src/main/java/com/github/maiky1304/chunkmaster/ChunkMaster.kt
@@ -0,0 +1,15 @@
+package com.github.maiky1304.chunkmaster
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class ChunkMaster : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+
+}
